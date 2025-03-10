fc9fddf07867112d9af29afaa8afe3a8bef581bc
mikeatok <miatokhala@s6c.mlp.college>
2025-03-10 21:02:38 +0000

    login and signup design is ready

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask,render_template
-PORT= 2100
+PORT= 2000
 from Cards import cards
 from signup import signin
+from login import logins
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-<<<<<<< HEAD
-	return render_template("signup.html" , signup=signin)
-=======
-	return render_template("index.html", Cards=cards)
->>>>>>> 457e9084bb38efa124828b2efbff6fb2af12fb04
-
+	return render_template("login.html",login=logins)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
---
+++ login.py
@@ -0,0 +1,9 @@
+logins= [{
+    "id": 1,
+    "body": "username",
+    
+},
+{
+    "id": 2,
+    "body":"password",
+}]
