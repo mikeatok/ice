8002e09e17cc3f308d5f7edf963cddfc8d551123
Sundeep Gurung <sugurung@s6c.mlp.college>
2025-03-10 11:17:47 +0000

    image

--- cards.py
+++ cards.py
@@ -3,7 +3,7 @@ cards = {
     "id": 1,
     "title": "New York Ranger up at half time by 2",
     "body": "",
-    "source": ""
+    "source": "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_334,q_75,w_579/v1/crm/newyorkstate/nyr_102910_0558_0f65df5c-fc60-af2b-6bf86cbeb2763490.jpg"
 },
     {
     "id": 2,
