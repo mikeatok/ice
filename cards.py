c9d2446b9343f17af286e4cefbde091798ccf24f
Sundeep Gurung <sugurung@s6c.mlp.college>
2025-03-10 11:08:53 +0000

    adding more cards

--- cards.py
+++ cards.py
@@ -1,6 +1,33 @@
 cards = {
+    {
     "id": 1,
+    "title": "New York Ranger up at half time by 2",
+    "body": "",
+    "source": ""
+},
+    {
+    "id": 2,
     "title": "",
     "body": "",
     "source": ""
+},
+    {
+    "id": 3,
+    "title": "",
+    "body": "",
+    "source": ""
+},
+    {
+    "id": 4,
+    "title": "",
+    "body": "",
+    "source": ""
+},
+    {
+    "id": 5,
+    "title": "",
+    "body": "",
+    "source": ""
+},
+
 }
