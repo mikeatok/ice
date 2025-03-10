457e9084bb38efa124828b2efbff6fb2af12fb04
GurungSun <sugurung@s6c.mlp.college>
2025-03-10 19:28:07 +0000

    updates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("login.html" ,login=logins)
+	return render_template("index.html", Cards=cards)
 
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
