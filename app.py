c4246826a01052a4763ed439e53c4f5f116c0f7e
mikeatok <miatokhala@s6c.mlp.college>
2025-03-10 19:22:41 +0000

    sign up page is ready

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template
 PORT= 2100
 from Cards import cards
+from login import logins
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("index.html", Cards=cards)
+	return render_template("login.html" ,login=logins)
 
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
---
+++ login.py
@@ -0,0 +1,15 @@
+logins= [{
+    "id": 1,
+    "body": "username",
+    
+},
+{
+    "id": 2,
+    "body":"password",
+},
+{
+    "id": 3,
+    "body":"confirm password",
+}
+
+]
