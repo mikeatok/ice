bb177a5ea17dea5b41a507fb20d8196ded1d3ad4
mikeatok <miatokhala@s6c.mlp.college>
2025-03-10 11:40:19 +0000

    added git ignore

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template
 PORT= 2100
+from cards import cards
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("index.html")
+	return render_template("index.html", Cards=cards)
 
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
