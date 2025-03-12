af13919f4974228630948b6d7f883a48ee2e1908
GurungSun <sugurung@s6c.mlp.college>
2025-03-12 21:18:25 +0000

    working on mstats

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 PORT= 2000
+from lineups import tLineup
 from Cards import cards
 from signup import signin
 from login import logins
@@ -8,7 +9,7 @@ app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("index.html",Cards=cards)
+	return render_template("mstats.html",lineups=tLineup)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
---
+++ lineups.py
@@ -0,0 +1,19 @@
+tLineup = [
+    {
+        "id": 1,
+        "teamName": "New York Rangers",
+        "fLineup": "Forwards: -Kyle Johnson -Tyler White -Sam Evans",
+        "dLineup": "Defence: -Chris Carters -Jake Wilson",
+        "gLineup": "Goaltender: Patrick Stevens",
+        "score": "1"
+    },
+
+    {
+        "id": 2,
+        "teamName": "Detroit Red Wings",
+        "fLineup": "Forwards: -Ethan Hayes -Matt Peterson -Aaron Foster",
+        "dLineup": "Defence: -Ryan Moore -Alex Cooper",
+        "gLineup": "Goaltender: Daniel Brooks",
+        "score": "1"
+    }
+]
