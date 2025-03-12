c3ce9470204cbfc1502ed9e91695223e29888086
GurungSun <sugurung@s6c.mlp.college>
2025-03-12 21:36:27 +0000

    working on mstats

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask,render_template
 PORT= 2000
-from lineups import tLineup
 from Cards import cards
 from signup import signin
 from login import logins
-
+from lineups import tLineup
 app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("mstats.html",lineups=tLineup)
+	return render_template("index.html",Cards=cards)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
