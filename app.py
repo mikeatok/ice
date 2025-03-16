f0ba0cf33612df22c9469803c0750123f0170805
GurungSun <sugurung@s6c.mlp.college>
2025-03-16 13:49:30 +0000

    added a trade database

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from Cards import cards
 from signup import signin
 from login import logins
 from weeklystatistics import weeklystats
+from trades import trade
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/") 
 def index(): 
-	return render_template("weeklystatistics.html",weeklystatistics=weeklystats)
+	return render_template("mstats2.html",lineups=tLineup)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
