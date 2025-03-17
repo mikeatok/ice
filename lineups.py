8b8ce4ccf340f94255274ca9e97f2964e661fc89
GurungSun <sugurung@s6c.mlp.college>
2025-03-17 12:11:51 +0000

    mstats changes

--- lineups.py
+++ lineups.py
@@ -1,19 +1,12 @@
 tLineup = [
     {
         "id": 1,
-        "teamName": "New York Rangers",
-        "fLineup": "Forwards: -Kyle Johnson -Tyler White -Sam Evans",
-        "dLineup": "Defence: -Chris Carters -Jake Wilson",
-        "gLineup": "Goaltender: Patrick Stevens",
-        "score": "1"
+        "source": "https://media.istockphoto.com/id/529964085/photo/mango.jpg?s=612x612&w=0&k=20&c=rmcMZlLOZFdn4NhUcjnaJ3EBHKYZQ4xH4xzpzFU4JgY=",
+        "title": "Will Cuylle body checks Alex DeBrincat",
     },
-
     {
         "id": 2,
-        "teamName": "Detroit Red Wings",
-        "fLineup": "Forwards: -Ethan Hayes -Matt Peterson -Aaron Foster",
-        "dLineup": "Defence: -Ryan Moore -Alex Cooper",
-        "gLineup": "Goaltender: Daniel Brooks",
-        "score": "1"
+        "source": "https://static01.nyt.com/images/2022/02/14/sports/14oly-hockey-parity1/14oly-hockey-parity1-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+        "title": "Will Cuylle body checks Alex DeBrincat",
     }
 ]
