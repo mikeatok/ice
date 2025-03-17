2fd9dee43adc246800e542fe8fe0bb22de35c384
Dunn <rdunn@s6c.mlp.college>
2025-03-17 11:58:00 +0000

    just keeping up to date

--- app.py
+++ app.py
@@ -10,11 +10,8 @@ from trades import trade
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-<<<<<<< HEAD
-	return render_template("settings.html")
-=======
-	return render_template("mstats2.html",lineups=tLineup)
->>>>>>> 01e23645f1328f8f45fa975cd72d84a66ef19909
+	return render_template("weeklystatistics.html",lineups=tLineup)
+
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
