18a95f7e134926cc8623291d2d6c9da966f67c72
GurungSun <sugurung@s6c.mlp.college>
2025-03-18 12:17:11 +0000

    fixed mstats bug

--- lineups.py
+++ lineups.py
@@ -1,12 +1,7 @@
 tLineup = [
     {
         "id": 1,
-        "source": "https://media.istockphoto.com/id/529964085/photo/mango.jpg?s=612x612&w=0&k=20&c=rmcMZlLOZFdn4NhUcjnaJ3EBHKYZQ4xH4xzpzFU4JgY=",
-        "title": "Will Cuylle body checks Alex DeBrincat",
-    },
-    {
-        "id": 2,
-        "source": "https://static01.nyt.com/images/2022/02/14/sports/14oly-hockey-parity1/14oly-hockey-parity1-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+        "source": "https://www.detroitnews.com/gcdn/authoring/authoring-images/2024/10/15/PDTN/75680760007-getty-images-2178454443.jpg?width=700&height=494&fit=crop&format=pjpg&auto=webp",
         "title": "Will Cuylle body checks Alex DeBrincat",
     }
 ]
