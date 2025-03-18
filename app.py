2caf1c30814eb8b9aedb28df2a54f57ed852eb35
mikeatok <miatokhala@s6c.mlp.college>
2025-03-18 14:44:41 +0000

    merging

--- app.py
+++ app.py
@@ -6,15 +6,10 @@ from signup import signin
 from login import logins
 from weeklystatistics import weeklystats
 from trades import trade
-
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-<<<<<<< HEAD
-	return render_template("settings.html")
-=======
-	return render_template("mstats2.html",lineups=tLineup)
->>>>>>> 01e23645f1328f8f45fa975cd72d84a66ef19909
+	return render_template("index.html",Cards=cards)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
