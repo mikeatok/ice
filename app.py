62fd43e84db7188647be0ed431b6541358333f39
Dunn <rdunn@s6c.mlp.college>
2025-03-20 13:55:25 +0000

    Finished ranking table

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ from settings import sett
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
+<<<<<<< Updated upstream
 	return render_template("settings2.html",settings=sett)
+=======
+	return render_template("weeklystatistics.html",Cards=cards)
+>>>>>>> Stashed changes
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
