6d364b10cec0a34bb6f7bbecc62b6cb69152f92b
GurungSun <sugurung@s6c.mlp.college>
2025-03-20 12:45:47 +0000

    settings

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from signup import signin
 from login import logins
 from weeklystatistics import weeklystats
 from trades import trade
+from settings import sett
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-	return render_template("index.html",Cards=cards)
+	return render_template("settings2.html",settings=sett)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
---
+++ settings.py
@@ -0,0 +1,22 @@
+sett=[
+    {
+        "id": 1,
+        "name": "Notifications",
+    },
+    {
+        "id": 1,
+        "name": "Dark Mode",
+    },
+    {
+        "id": 1,
+        "name": "Contact",
+    },
+    {
+        "id": 1,
+        "name": "Rate App",
+    },
+    {
+        "id": 1,
+        "name": "Log Out",
+    },
+]
