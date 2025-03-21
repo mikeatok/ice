115df38f4ea02ad19a28d8a1a1b595b7c9819eea
mikeatok <miatokhala@s6c.mlp.college>
2025-03-21 08:26:13 +0000

    index pag top bar  is set

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-PORT= 2000
+PORT= 2100
 from lineups import tLineup
 from Cards import cards
 from signup import signin
@@ -10,11 +10,7 @@ from settings import sett
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-<<<<<<< Updated upstream
-	return render_template("settings2.html",settings=sett)
-=======
-	return render_template("weeklystatistics.html")
->>>>>>> Stashed changes
+	return render_template("index.html", Cards=cards)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
