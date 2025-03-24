cc152648b8ba5e17c2de45c9e6bf3f0402e77b40
mikeatok <miatokhala@s6c.mlp.college>
2025-03-24 10:50:32 +0000

    made some changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from settings import sett
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-	return render_template("index.html", Cards=cards)
+	return render_template("login.html",login=logins)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
