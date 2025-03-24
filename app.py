418b14b8af2d45bfe2eae4fb5261145044138f14
Dunn <rdunn@s6c.mlp.college>
2025-03-24 11:24:28 +0000

    Card

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__,template_folder='template')
 def index(): 
 	return render_template("weeklystatistics.html", weeklystatistics=weeklystats)
 
+
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
 	app.register_blueprint("home_bp")
