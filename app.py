0a6c91ee49c26fb0ae7468ca7d62bcbea75aaace
mikeatok <miatokhala@s6c.mlp.college>
2025-03-31 18:53:36 +0100

    merging

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-PORT= 2000
+PORT= 3000
 from lineups import tLineup
 from Cards import cards
 from signup import signin
@@ -10,7 +10,6 @@ from settings import sett
 app = Flask(__name__,template_folder='template')
 @app.route("/") 
 def index(): 
-	return render_template("login2.html",login=logins)
 	return render_template("index.html",Cards=cards)
 if __name__ == "__main__": 
 	app.run(debug=True, host = '0.0.0.0', port=PORT) 
